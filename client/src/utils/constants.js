2b848ae20a5c48b71fd28baf31152d29922697d0
Joeumar Souza <joeumar@gmail.com>
2020-03-01 13:51:10 -0300

    Removing logs

--- client/src/utils/constants.js
+++ client/src/utils/constants.js
@@ -1,2 +1,1 @@
-// export const SERVER_BASE_URL = 'https://fathomless-fjord-77647.herokuapp.com/movies';
-export const SERVER_BASE_URL = 'http://localhost.com:3001/movies';
+export const SERVER_BASE_URL = 'https://fathomless-fjord-77647.herokuapp.com/movies';
