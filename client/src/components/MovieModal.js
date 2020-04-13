c6c753865ce8f65211ae157bf944771d921350e6
Joeumar Souza <joeumar@gmail.com>
2020-04-13 14:44:16 -0300

    Adding fade-in effect

--- client/src/components/MovieModal.js
+++ client/src/components/MovieModal.js
@@ -1,38 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import imdblogo from '../imdb-logo.png';
+
 const MovieModal = ({ isVisible, hide, movieInfo, imageBaseUrl }) =>
   isVisible
     ? renderModalContent({ hide, movieInfo, imageBaseUrl })
     : null;
 
-const InfoRow = (info, content) => (
-  <span className="info-row">
-    <h2 className="heading-secondary">{info}</h2>
-    <p className="paragraph">
-      {content}
-    </p>
-  </span>
-)
-
 const renderModalContent = ({ hide, movieInfo, imageBaseUrl }) => ReactDOM.createPortal(
   <>
-    <div className="modal-overlay" />
-    <div className="modal-wrapper" aria-modal aria-hidden tabIndex={-1} role="dialog">
-      <div className="modal">
-        <div className="modal-header">
-          <h2 className="heading-secondary">{movieInfo.title}</h2>
-          <button type="button" className="modal-close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
-            <span aria-hidden="true">&times;</span>
-          </button>
-        </div>
-        <img src={`${imageBaseUrl}${movieInfo.backdrop_path}`} alt={movieInfo.title} />
-        {InfoRow('Original language:', movieInfo.original_language)}
-        {InfoRow('Rating:', movieInfo.vote_average)}
-        {InfoRow('Budget: $USD:', movieInfo.budget)}
-        {InfoRow('Votes:', movieInfo.vote_count)}
-        {InfoRow('Release date:', movieInfo.release_date)}
-        {InfoRow('Sinopse:', movieInfo.overview)}
+    <div className="modal" aria-modal aria-hidden tabIndex={-1} role="dialog">
+      <div className="modal__content">
+
+        <button type="button" className="modal__close-button" data-dismiss="modal" aria-label="Close" onClick={hide}>
+          <span aria-hidden="true">&times;</span>
+        </button>
+
+        <section className="modal__header">
+          <div className="modal__title">{movieInfo.title}</div>
+          <div className="modal__release-date">{`- (${movieInfo.release_date.slice(0, 4)})`}</div>
+          <div className="modal__vote-average">{movieInfo.vote_average} &#9733;</div>
+          <div className="modal__vote-count">{movieInfo.vote_count} - votes</div>
+        </section>
+
+        <section className="modal__info">
+          <img className="modal__backdrop" src={`${imageBaseUrl}${movieInfo.backdrop_path}`} alt={movieInfo.title} />
+
+          <div className="modal__overview">
+            <p>{movieInfo.overview}</p>
+            <p>Original Language: {movieInfo.original_language}</p>
+            <p className="modal__imdb-link">Open with<img className="modal__imdb-logo" src={imdblogo} alt="IMDb" onClick={() => window.open(`https://www.imdb.com/title/${movieInfo.imdb_id}`)} /></p>
+          </div>
+        </section>
+
       </div>
     </div>
   </>, document.body
